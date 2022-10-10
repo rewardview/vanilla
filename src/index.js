51236e68f533ea0262880436d33c078edbb1e86d
최가오 <sadb0101@naver.com>
2022-10-10 15:59:41 +0900

    feat : 기본 기능 추가 완료

--- src/index.js
+++ src/index.js
@@ -1,108 +1,69 @@
 
 import './styles.css'
 
-var addButton = document.getElementById('btnAdd')
-addButton.addEventListener('click', handleClickAddButton)
+var btnAdd = document.getElementById('btnAdd')
+btnAdd.addEventListener('click', handleClickAddButton)
+
+var btnDelSel = document.getElementById('btnDelSel')
+btnDelSel.addEventListener('click', handleClickDelSel)
+
+var btnDelLast = document.getElementById('btnDelLast')
+btnDelLast.addEventListener('click', handleClickDelLast)
+
+var btnDelAll = document.getElementById('btnDelAll')
+btnDelAll.addEventListener('click', handleClickDelAll)
 
 function handleClickAddButton() {
   var todoTextInput = document.getElementsByClassName('text-basic')
   var todoText = todoTextInput.item(0).value
+  if (todoText) {
+    var listBody = document.getElementById('listBody')
 
-  var listBody = document.getElementById('listBody')
+    var newList = document.createElement('tr')
+    var newInputTd = document.createElement('td')
 
-  var newList = document.createElement('tr')
-  var newInputTd = document.createElement('td')
+    var newInput = document.createElement('input')
+    newInput.type = 'checkbox'
+    newInput.className = 'btn-chk'
 
-  var newInput = document.createElement('input')
-  newInput.type = 'checkbox'
-  newInput.class = 'btn-chk'
+    var newTextTd = document.createElement('td')
+    var textNode = document.createTextNode(todoText)
+    newTextTd.appendChild(textNode)
 
-  var newTextTd = document.createElement('td')
-  var textNode = document.createTextNode(todoText)
-  newTextTd.appendChild(textNode)
+    newInputTd.appendChild(newInput)
 
-  newInputTd.appendChild(newInput)
+    newList.appendChild(newInputTd)
+    newList.appendChild(newTextTd)
 
-  newList.appendChild(newInputTd)
-  newList.appendChild(newTextTd)
-
-  listBody.appendChild(newList)
-  todoTextInput.item(0).value = ''
+    listBody.appendChild(newList)
+    todoTextInput.item(0).value = ''
+  }
 }
 
 
+function handleClickDelSel() {
+  var listBody = document.getElementById('listBody')
 
-// var main = document.getElementById('main')
-// function init() {
-//   var box = document.createElement('div')
-//   box.className = 'list-box'
+  var checkedBox = document.getElementsByClassName('btn-chk')
+  for (let index = checkedBox.length - 1; index >= 0; index--) {
+    const element = checkedBox[index];
+    element.checked && listBody.deleteRow(index)
+  }
+}
 
-//   var title = createTitle()
-//   var writeBox = createWriteBox()
-//   // var todoTable = createTodoTable()
-//   box.appendChild(title)
-//   box.appendChild(writeBox)
-//   // box.appendChild(todoTable)
+function handleClickDelLast() {
+  var listBody = document.getElementById('listBody')
 
-//   main.appendChild(box)
-// }
+  var checkedBox = document.getElementsByClassName('btn-chk')
 
+  listBody.deleteRow(checkedBox.length - 1)
+}
 
-// function createTitle() {
-//   var title = document.createElement('h1')
-//   title.innerText = 'To Do List'
-//   return title
-// }
+function handleClickDelAll() {
+  var listBody = document.getElementById('listBody')
 
-// function createWriteBox() {
-//   var writeBox = document.createElement('div')
-//   writeBox.className = 'write-box'
-//   var textInput = document.createElement('input')
-//   textInput.className = 'text-basic'
-//   var addButton = document.createElement('button')
-//   addButton.id = 'btnAdd'
-//   addButton.addEventListener('click', handleClickAddButton)
-//   return writeBox
-// }
-// function createTodoTable() {
-//   var todoTable = document.createElement('table')
-//   todoTable.className = 'list-table'
-//   var colgroup = document.createElement('colgroup')
-//   var col1 = document.createElement('col')
-//   col1.style.width = '10%'
-//   colgroup.appendChild(col1)
-//   var col2 = document.createElement('col')
-//   col2.style.width = '90%'
-//   colgroup.appendChild(col2)
-//   todoTable.appendChild(colgroup)
-
-//   //  <table class="list-table">
-//   //    <colgroup>
-//   //      <col width="10%" />
-//   //      <col width="90%" />
-//   //    </colgroup>
-//   //    <thead>
-//   //      <tr>
-//   //        <th>check</th>  
-//   //        <th>To do List</th>
-//   //      </tr>
-//   //    </thead>
-//   //    <tbody id="listBody">
-//   //      <tr>
-//   //        <td><input type="checkbox" class="btn-chk" /></td>
-//   //        <td>청소하기</td>
-//   //      </tr>
-//   //      <tr>
-//   //        <td><input type="checkbox" class="btn-chk" /></td>
-//   //        <td>숙제하기</td>
-//   //      </tr>
-//   //    </tbody>
-//   //  </table>
-
-//   return todoTable
-// }
-// init()
-
-// function handleClickAddButton() {
-
-// }
+  var checkedBox = document.getElementsByClassName('btn-chk')
+  for (let index = checkedBox.length - 1; index >= 0; index--) {
+    listBody.deleteRow(index)
+  }
+}
